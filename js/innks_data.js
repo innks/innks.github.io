addfbcdbe8c3ae817692f157df1bc64562f37c28
iinks <rain5x@naver.com>
2017-01-21 18:32:46 +0900

    Site updated: 2017-01-21 18:32:45

---
+++ js/innks_data.js
@@ -0,0 +1,15 @@
+// Version : 201612271
+
+var VersionBlog = "no PROJECT";
+var VersionBuild = "v.6.0.1";
+var Buildfirst = "2017.01.10"
+var BuildUpdate = "2017. 01. 13";
+var BuildUpdateMemo = " ";
+
+var innksSkin = "v6.0.1";
+var hexoPackage = "3.2.2";
+var npmVer = "3.10.10";
+var nodejsVer = "6.9.4";
+var treeVer ="0.4.9";
+var JQver = "2.0.3";
+var FontAwesomeVer = "4.6.3";
